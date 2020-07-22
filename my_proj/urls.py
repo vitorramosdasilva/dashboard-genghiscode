123cbbefdba8611d661d234c8523222e5122c4d8
vitorramosdasilva <rsvitor_81@hotmail.com>
2020-07-22 14:43:30 -0300

    make it better

--- my_proj/urls.py
+++ my_proj/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('dashboard.urls')),
